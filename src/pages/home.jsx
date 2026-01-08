2c749675c029c1f43011dd710da59f2c0189bc2a
sanaht77 <sanahtatla@gmail.com>
2026-01-07 20:04:21 -0800

    major redesign

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,46 +3,61 @@ import Typed from "typed.js";
 import { Link } from "react-router-dom";
 import "./homeStyles.css";
 import Particle from "../components/particle.jsx";
+import Aurora from '../components/aurora.jsx';
 
 export default function Home() {
 
-    useEffect(() => {
-
-        document.getElementById("title").classList.add("show");
-        document.querySelector(".auto-type").classList.add("show");
-        document.getElementById("links").classList.add("show")
-
-        const typed = new Typed(".auto-type", {
+  useEffect(() => {
+    const typed = new Typed(".auto-type", {
         strings: ["Computer Science Student", 
-            "Developer", 
-            "Techincal Intern"],
+                  "Aspiring Developer", 
+                  "I love My Dog"],
         typeSpeed: 75,
         backSpeed: 75,
         loop: true,
-
     });
-
     return () => typed.destroy();
   }, []);
 
   return (
+  <div className="page">
 
-  <div style={{ position: "relative" }}>
-    <Particle />
+    <div className="bg">
+      <Aurora
+      colorStops={["#9b0d81","#6f51e6","#4405a3"]}
+      amplitude={0.7}
+      blend={0.65}
+    />
+    </div>
+    
+    
+    <div className="content">
 
-    <div className="container">
-      <h1 id="title" className="header">Hi. I'm Sanah.</h1>
+      <section className="section home">
+        <h1 className="title">Hey, I'm Sanah!</h1>
+        <div className="subtitle">
+          <span className="auto-type"></span>
+        </div>
+      </section>
 
-      <div className="subtitle">
-        <h2 className="auto-type subheader"></h2>
-      </div>
+      <section className="section about">
+        <p className="paragraph">  I'm Sanah, a second year computer science student at Simon Fraser University. 
+          I began programming on my high school VEX Robotics team, 
+          where I combined technical skills with teamwork to design 
+          my own PID algorithm. As a Computer Science student, I've 
+          built a strong foundation in data structures and algorithms 
+          while developing proficiency in C++, C, React, HTML, and CSS. 
+          Beyond coursework, I've explored personal projects that 
+          strengthened my problem-solving and creativity. I'm eager to 
+          keep learning and apply my skills to building impactful software.
+        </p>
 
-      <div id="links">
-        <Link className="zoom body" to="/projects">See my projects</Link>
-        <Link className="zoom body" to="/about">Learn about me</Link>
-        <Link className="zoom body" to="/resume">View my resume</Link>
+      </section>
 
-      </div>
+
+
+
+
     </div>
     </div>
     
